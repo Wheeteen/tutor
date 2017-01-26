29090b80ea3aa25e3a8ae23e5d02808b56beddaf
yinzishao <370953598@qq.com>
2017-01-26 20:52:11 +0800

    order报名和邀请实现上传截图后才finished，不能重复邀请和一些错误操作的处理

--- api/bll/order.py
+++ api/bll/order.py
@@ -48,14 +48,29 @@ def applyParent(request):
         teacher = teachers[0]
         pd = pds[0]
         if method == 1:
-            #老师报名家长
-            #老师可以报名多个家长!!
+            #老师报名家长，老师可以报名多个家长!!
+
+            #检查家长是否已经邀请该老师，是否已经存在两个人的对应订单
+            orders = OrderApply.objects.filter(apply_type=2,pd=pd,tea=teacher)
+            if len(orders):
+                return JsonError(u"该家长已经邀请了您！")
+
+            #家长之前拒绝了该老师，老师再次报名家长
+            orders = OrderApply.objects.filter(apply_type=1,pd=pd,tea=teacher)
+            if len(orders):
+                order = orders[0]
+                order.finished = 0
+                order.parent_willing = 1
+                order.teacher_willing =1
+                order.update_time = timezone.now()
+                order.expectation = expectation
+            else:
+                order = OrderApply(apply_type=1, pd=pd,tea=teacher,parent_willing=1,teacher_willing=1,
+                               pass_not=1,update_time=timezone.now(),expectation=expectation,finished=0)
             #事务
             try:
                 with transaction.atomic():
                     #新建订单
-                    order = OrderApply(apply_type=1, pd=pd,tea=teacher,parent_willing=1,teacher_willing=2,
-                                       pass_not=1,update_time=timezone.now(),expectation=expectation,finished=0)
                     order.save()
                     message_title = teacher.name + u"向您报名!"
                     message_content = teacher.name + u"向您报名!请到“我的老师”处查看详细信息!"
@@ -73,7 +88,7 @@ def applyParent(request):
             try:
                 with transaction.atomic():
                     #删除订单
-                    orders = OrderApply.objects.filter(apply_type=1, pd=pd,tea=teacher)
+                    orders = OrderApply.objects.filter(apply_type=1, pd=pd,tea=teacher,finished=0)
                     if len(orders):
                         order = orders[0]
                         order.delete()
@@ -117,16 +132,29 @@ def inviteTeacher(request):
         teacher = teachers[0]
         if method == 1 :
             #家长邀请老师
-            #如果家长已经邀请了老师，返回错误
-            oa = OrderApply.objects.filter(apply_type=2, pd=parentorder)
+            #如果家长已经邀请了其他老师，并且未完全，返回错误
+            oa = OrderApply.objects.filter(apply_type=2, pd=parentorder,finished=0)
             if len(oa) != 0:
-                return JsonError(u"已经邀请了老师!")
+                return JsonError(u"您有未完成处理的订单！")
+            #如果老师已经报名了该家长
+            orders = OrderApply.objects.filter(apply_type=1,pd=parentorder,tea=teacher)
+            if len(orders):
+                return JsonError(u"该老师已经向您报名了！")
+            #老师之前拒绝了该家长，家长再次报名老师
+            orders = OrderApply.objects.filter(apply_type=2,pd=parentorder,tea=teacher)
+            if len(orders):
+                order = orders[0]
+                order.finished = 0
+                order.parent_willing = 2
+                order.teacher_willing = 1
+                order.update_time = timezone.now()
+            else:
+                order = OrderApply(apply_type=2, pd=parentorder,tea=teacher,parent_willing=2,teacher_willing=1,
+                                       pass_not=1,update_time=timezone.now(),finished=0)
             #事务
             try:
                 with transaction.atomic():
                     #新建订单
-                    order = OrderApply(apply_type=2, pd=parentorder,tea=teacher,parent_willing=2,teacher_willing=1,
-                                       pass_not=1,update_time=timezone.now(),finished=0)
                     order.save()
                     message_title = parentorder.name + u"向您发起了邀请!"
                     message_content = parentorder.name + u"向您发起了邀请!请到“我的家长”处查看详细信息!"
@@ -142,7 +170,7 @@ def inviteTeacher(request):
             try:
                 with transaction.atomic():
                     #取消订单
-                    orders = OrderApply.objects.filter(apply_type=2, pd=parentorder,tea=teacher)
+                    orders = OrderApply.objects.filter(apply_type=2, pd=parentorder,tea=teacher,finished=0)
                     if len(orders):
                         order = orders[0]
                         order.delete()
@@ -166,6 +194,7 @@ def judge(teach_willing,result):
     elif teach_willing == 0:
         result = u"拒绝"
     return result
+
 @login_required()
 @api_view(['GET'])
 @authentication_classes((CsrfExemptSessionAuthentication, BasicAuthentication))
@@ -278,6 +307,7 @@ def handleOrder(request):
                         message_content = pd.name + u"接受了你的报名！请到“我的家长”处查看详细信息!"
                     else:
                         order.parent_willing = 0
+                        order.finished = 1
                         message_title = pd.name + u"拒绝了你的报名！"
                         message_content = pd.name + u"拒绝了你的报名！请到“我的家长”处查看详细信息!"
                     #新建消息
@@ -285,7 +315,6 @@ def handleOrder(request):
                     try:
                         with transaction.atomic():
                             message.save()
-                            order.finished = 1
                             order.save()
                         #TODO:消息推送到微信端
                     except Exception,e:
@@ -297,12 +326,14 @@ def handleOrder(request):
                 return JsonError(u"处理错误，请确定数据无误！")
         else:
         #老师处理家长
+        # 第一种情况：老师处理家长的邀请
+        # 第二种情况：老师报名家长后，家长同意后，老师再次处理同意
             teas = user.teacher_set.all()
             parentorders = ParentOrder.objects.filter(pd_id = id)
             if ( len(parentorders ) and  len(teas) ):
                 pd = parentorders[0]
                 tea = teas[0]
-                orders = OrderApply.objects.filter(apply_type=2, tea=tea, pd=pd)
+                orders = OrderApply.objects.filter(apply_type=2, tea=tea, pd=pd,finished=0)
                 if len(orders) :
                     order = orders[0]
                     #对订单进行处理
@@ -312,13 +343,13 @@ def handleOrder(request):
                         message_content = tea.name + u"接受了你的邀请！请到“我的老师”处查看详细信息!"
                     else:
                         order.teacher_willing = 0
+                        order.finished = 1
                         message_title = tea.name + u"拒绝了你的邀请！"
                         message_content = tea.name + u"拒绝了你的邀请！请到“我的老师”处查看详细信息!"
                     message = Message(sender=user, receiver=pd.wechat, message_title=message_title, message_content=message_content,status=0)
                     try:
                         with transaction.atomic():
                             message.save()
-                            order.finished = 1
                             order.save()
                         #消息推送到微信端
                     except Exception,e:
