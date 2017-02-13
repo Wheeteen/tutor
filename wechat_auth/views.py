b47ca1d6d1fb3a7997d2ba1e0bf46a434181c717
yinzishao <370953598@qq.com>
2017-02-13 21:51:53 +0800

    管理员添加WeChatinfo

--- wechat_auth/views.py
+++ wechat_auth/views.py
@@ -204,4 +204,15 @@ def login_admin(request):
     name = 'yinzishao'
     user = authenticate(username=name, password=name)
     login(request,user)
+    request.session['info'] = {
+            'province': 'Guangdong',
+            'openid': 'odE4WwK3g05pesjOYGbwcbmOWTnc',
+            'headimgurl': 'http://wx.qlogo.cn/mmopen/fR41VbicrntibxhNY3WfaKgHBTbe1d6Gz0tPjhHpicwJerJiaAictfHiaLiaqCcVIs5EKOzsD4yaiadyUIUHK2Lu07K9EqArtialVJd4b/0',
+            'language': 'zh_CN',
+            'city': 'Yunfu',
+            'country': 'CN',
+            'sex': 1,
+            'privilege': [],
+            'nickname': u'\u5c39\u5b50\u52fa'
+        }
     return redirect('/getNum/')
